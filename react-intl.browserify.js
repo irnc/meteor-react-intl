0ea95b807b19ecf307d07ac05c4eeb1dd25a5a6b
Pavel Zubkou <pavel.zubkou@gmail.com>
2015-11-24 00:59:34 +0300

    Require English and Russian locales into client builds

--- react-intl.browserify.js
+++ react-intl.browserify.js
@@ -1,2 +1,7 @@
 Intl = require('intl');
+
+require('intl/locale-data/jsonp/en');
+require('intl/locale-data/jsonp/ru');
+
 ReactIntl = require('react-intl');
+ReactIntl.addLocaleData(require('react-intl/dist/locale-data/ru'));
